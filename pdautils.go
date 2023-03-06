6e015f16efc62fc8a5decfc31b8c08c873d654b1
Norbaeocystin <baranrastislav@gmail.com>
2023-03-06 12:36:40 -0300

    pda updated

--- pdautils.go
+++ pdautils.go
@@ -4,7 +4,10 @@ package gorca
 TODO implement logic from https://github.com/orca-so/whirlpools/blob/main/sdk/src/utils/public/pda-utils.ts
 */
 
-import "github.com/gagliardetto/solana-go"
+import (
+	"encoding/binary"
+	"github.com/gagliardetto/solana-go"
+)
 
 const PDA_WHIRLPOOL_SEED = "whirlpool"
 
@@ -24,10 +27,28 @@ func GetOracle(programID, whirlpooAddress solana.PublicKey) (solana.PublicKey, u
 }
 
 // TODO implement
-//func GetWhirlPool() {}
-//func GetPosition() {}
-//func GetPositionMetadata() {}
-//func GetTickArray(){}
+func GetWhirlPool(programID, configKey, mintA, mintB solana.PublicKey, tickSpacing uint16) (solana.PublicKey, uint8, error) {
+	buff := [2]byte{}
+	binary.LittleEndian.PutUint16(buff[:], tickSpacing)
+	address, bump, err := solana.FindProgramAddress([][]byte{[]byte(PDA_WHIRLPOOL_SEED), configKey.Bytes(), mintA.Bytes(), mintB.Bytes(), buff[:]}, programID)
+	return address, bump, err
+}
+
+func GetPosition(programID, positionMintKey solana.PublicKey) (solana.PublicKey, uint8, error) {
+	address, bump, err := solana.FindProgramAddress([][]byte{[]byte(PDA_POSITION_SEED), positionMintKey.Bytes()}, programID)
+	return address, bump, err
+}
+
+//func GetPositionMetadata(positionMintKey solana.PublicKey) (solana.PublicKey, uint8, error) {
+//	address, bump, err := solana.FindProgramAddress([][]byte{[]byte(PDA_METADATA_SEED), METADATA_PROGRAM_ADDRESS.Bytes(), positionMintKey.Bytes()}, METADATA_PROGRAM_ADDRESS)
+//	return address, bump, err
+//}
+
+func GetTickArrayViaFPA(programID, whirlpoolAddress solana.PublicKey, startTick int64) (solana.PublicKey, uint8, error) {
+	address, bump, err := solana.FindProgramAddress([][]byte{[]byte(PDA_TICK_ARRAY_SEED), whirlpoolAddress.Bytes(), []byte(string(startTick))}, programID)
+	return address, bump, err
+}
+
 //func GetTickArrayFromTickIndex(){}
 //func GetTickArrayFromSqrtPrice(){}
 //func GetFeeTier(){}
