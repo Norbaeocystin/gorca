ff807711c1460cd0b63e4a91dccd11240995957c
Norbaeocystin <baranrastislav@gmail.com>
2023-06-07 07:51:39 -0400

    all function

--- position.go
+++ position.go
@@ -44,6 +44,41 @@ func FindOrcaPositionsForOwner(client *rpc.Client, owner, pool solana.PublicKey)
 	return positions, nil
 }
 
+func FindOrcaPositionsForOwner(client *rpc.Client, owner solana.PublicKey) ([]PositionData, error) {
+	positions := make([]PositionData, 0)
+	tokens, err := client.GetTokenAccountsByOwner(context.TODO(), owner,
+		&rpc.GetTokenAccountsConfig{
+			Mint:      nil,
+			ProgramId: solana.TokenProgramID.ToPointer(),
+		},
+
+		&rpc.GetTokenAccountsOpts{
+			Commitment: "",
+			Encoding:   solana.EncodingBase64,
+			DataSlice:  nil,
+		})
+	if err != nil {
+		log.Println("fetching positions error", err)
+	}
+	for _, tk := range tokens.Value {
+		var ta token.Account
+		borshDec := bin.NewBorshDecoder(tk.Account.Data.GetBinary())
+		// log.Println(tk.Pubkey.String(), ta.Mint)
+		borshDec.Decode(&ta)
+		// log.Println(tk.Pubkey.String(), ta.Mint, len(tk.Account.Data.GetBinary()))
+		if ta.Amount == 1 {
+			// tkPubkey - positionTokenAccount, ta.Mint - positionMint
+			// log.Println(tk.Pubkey, ta.Mint)
+			pk, _, _ := GetPosition(ORCA_WHIRPOOL_PROGRAM_ID, ta.Mint)
+			position := GetPositionData(client, pk)
+			if position.Whirlpool != nil {
+				positions = append(positions, position)
+			}
+		}
+	}
+	return positions, nil
+}
+
 func GetPositionData(client *rpc.Client, position solana.PublicKey) PositionData {
 	account, _ := client.GetAccountInfoWithOpts(context.TODO(),
 		position,
